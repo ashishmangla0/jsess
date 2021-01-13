5fd14e9fda171bbad8b20c0974ad48518631e8ec
ashish <ashishmangla1991@gmail.com>
2021-01-13 21:19:21 +0530

    add new folders

---
+++ 03_08/main.js
@@ -0,0 +1,50 @@
+var pens;
+pens = ["red", "blue", "green", "orange"];
+
+console.log("Before:" , pens);
+
+// length of array
+//console.log(pens.length);
+
+//methods in Array
+ 
+// reverse
+//console.log(pens.reverse());
+
+//remove the first value of the array
+//console.log(pens.shift());
+
+//add new item to  the front of  first value of the array
+//console.log(pens.unshift("purple","black"));
+
+//remove th last item
+//pens.pop();
+
+//add elements at the end og array
+//pens.push("pink","prussian blue")
+
+//remove elements from a position
+//pens.splice(pos,n)
+//pens.splice(2,1); //remove one element
+//pens.splice(2,2); //remove two elements
+
+//console.log("after:" , pens);
+
+
+
+
+
+
+// methods that  create a copy of array
+// var newPens = pens.slice()
+// console.log("new pens : ", newPens);
+
+//var result = pens.indexOf(searc,index);
+// var result = pens.indexOf("orange",1);
+// console.log("The index position is: ", result );
+
+var arrayString = pens.join(" | ");
+console.log("String from array: ", arrayString);
+
+
+
