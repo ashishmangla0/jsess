597ca6047e2d84962ae68896f2fca31b1b81e361
ashish <ashishmangla1991@gmail.com>
2021-01-13 09:46:45 +0530

    add condition

---
+++ 03_05/main.js
@@ -0,0 +1,10 @@
+var a = 5;
+var b = 5;
+var theNumberMatch ;
+if( a===b ){
+    theNumberMatch = true;
+} else{
+    theNumberMatch = false;
+}
+
+console.log(`The numbers match: ${theNumberMatch}`);
