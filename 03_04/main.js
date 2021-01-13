cb6f235b7ccb2dcc1df90a33851e22b4a6c542df
ashish <ashishmangla1991@gmail.com>
2021-01-13 08:51:50 +0530

    op

---
+++ 03_04/main.js
@@ -0,0 +1,17 @@
+var a = 4;
+var b = "5";
+sum = a + b;
+sub = a - b;
+mul = a*b;
+divide = a/b; 
+var hamanSum = `The value of a: ${a}. The value of b: ${b}. The sum of a and b: ${sum}.`;
+console.log(hamanSum);
+
+var humansub = `The value of a: ${a}. The value of b: ${b}. The sub of a and b: ${sub}.`;
+console.log(humansub);
+
+var humanmul = `The value of a: ${a}. The value of b: ${b}. The mul of a and b: ${mul}.`;
+console.log(humanmul);
+
+var humandiv = `The value of a: ${a}. The value of b: ${b}. The mul of a and b: ${divide}.`;
+console.log(humandiv);
