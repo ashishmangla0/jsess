1aa5c89f974e0ecd06586c408c77b8bf016d1dba
ashish <ashishmangla1991@gmail.com>
2021-01-13 08:31:29 +0530

    add assignment

---
+++ 03_03/main.js
@@ -0,0 +1,43 @@
+/*********** 
+ *  Standard operators (+, -, =, *, /)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  ******** */
+
+ var a = 5;
+ var b = 4;
+ var sum = a+b;
+ var subtract = a - b;
+ var multiply = a * b;
+ var divide = a/b;
+
+ /*****
+  * oprator Presedence
+  * var result = a + (b*c);
+  * var result2 = (a + b) * c;
+  * ***** */
+
+  /**********
+   * shothand assignemnt
+   * a = 5;
+   * a += 4;
+   * *********** */
+
+   /**********
+    * var a =5;
+    * a++ /Grab the original value, then add 1 to it/
+    * ++a Add one to the value
+    * 
+    * 
+    * 
+    * ********** */
+
+
+
+    var r = 5;
+    console.log(++r); //add 1 to the value;
+    console.log(r++) // grab r value and add 1 to it
